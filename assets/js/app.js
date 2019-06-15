35052b8689d59e2f6fbc46c36d2b9c3c7908bce8
icepants <shawnluther99@gmail.com>
2019-06-15 13:26:46 -0400

    Performed a JS code cleanup in app.js, formatted all of the JS code for readability.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,86 +1,91 @@
-
-$("#btnSubmit").on('click', function(e) {
+// When the user clicks on the submit button, populate park results to the datatable 'parkPopulation'.
+$("#btnSubmit").on('click', function (e) {
+    // Prevent Page Reloading.
     e.preventDefault();
+    // Check to see if we're hitting our click.
+    console.log("trying to make a table");
+    // Execute API response data population.
+    populateSearchResults();
+    // Hide home page carousel.
     document.getElementById("carouselContainer").style.display = "none";
+    // Show Park Population Table.
     document.getElementById("parkPopulation").style.display = "block";
 })
 
-    // Array for parks 
-    var nationalParks = [
-        
-    ]; 
+// This array contains the names of our National Parks.
+var nationalParks = [
 
-    // Var for Zip Code 
-    var zipcode = ""; 
+];
 
-    // Vars for query URL 
-    // api key
-    var apiKey = ""
-    //query URL 
-    var parkQueryURL = "http://developer.nps.gov/api/v1/parks?stateCode=PA&limit=10&api_key=TUzbrDxmdtDfjNLGofmsOXAmQ6WPMwukeORXBBHm"; 
-    
+// This variable contains zip codes.
+var zipcode = "";
 
-    parkFetcher(); 
+// Here we define the variables for our queryURL.
 
-    function parkFetcher () { 
-        console.log("parkFetcher");
+// api key
+var searchState = "";
+//query URL 
+var parkQueryURL = "https://developer.nps.gov/api/v1/parks?stateCode=PA&limit=10&api_key=TUzbrDxmdtDfjNLGofmsOXAmQ6WPMwukeORXBBHm";
 
-        $.ajax ({
-            url:parkQueryURL, 
-            method: "GET"
-        })
+// This calls our function that fetches the API data from our ajax response object.
+parkFetcher();
 
-        .then(function (response){
-            var parks = response.data; 
-            console.log(parks); 
+// Ajax API Park Fetcher.
+function parkFetcher() {
+    // Check to see if we're hitting this function.
+    console.log("parkFetcher");
 
+    // Our ajax request.
+    $.ajax({
+        url: parkQueryURL,
+        method: "GET"
+    })
+
+        // Get our response data.
+        .then(function (response) {
+            var parks = response.data;
+            // console.log(parks); 
+
+            // Loop through our National Parks Array.
             for (var i = 0; i < parks.length; i++) {
-                var currentPark =  parks[i]
-                console.log('currentPark',currentPark.latLong)
-                if(currentPark.latLong === ''){
-                    i++;
-                }else {
+                var currentPark = parks[i]
+                console.log('currentPark', currentPark.latLong);
+                // If the parks are empty, just continue.
+                if (currentPark.latLong === '') {
+                    i++; // Keep it movin'!
+                } else {
+                    // Otherwise, push data.
                     var splitValue = currentPark.latLong.split(", ");
                     var lat = splitValue[0].split(":")[1];
                     var long = splitValue[1].split(":")[1];
+                    // Push data.
                     nationalParks.push({
-                        name:currentPark.fullName,
-                        type:currentPark.designation,
+                        name: currentPark.fullName,
+                        type: currentPark.designation,
                         lat,
                         long,
-                        url:currentPark.url,
+                        url: currentPark.url,
                     });
                 }
-                    console.log(nationalParks);
+                // Log Push.
+                console.log(nationalParks);
             }
 
-        }) 
-        .then(populateSearchResults)
-    }
-
-    function populateSearchResults () {
-        console.log(nationalParks,'nationalParks');
-        nationalParks.forEach(function(nationalPark){
-            var newRow = $("<tr>").append (
-                    $("<td>").text(nationalPark.name),
-                    $("<td>").text(nationalPark.designation),
-                    $("<td>").text(nationalPark.url)
-                );
-             $(".searchResults > tbody").append(newRow)
         })
-    };
+    // .then(populateSearchResults)
+}
 
-    // function populateSearchResults () {
-    //     var newRow = $("<tr>").append (
-    //         $("<td>").text(this.nationalParks.name),
-    //         $("<td>").text(this.nationalParks.designation),
-    //         $("<td>").text(this.nationalParks.url), 
-    //         console.log ("accessing populate search results function"), 
-    //     ); 
-    
-    //     $(".searchResults > tbody").append(newRow); 
-    // }; 
-
-
-    
-
+// Populate our datatable with our park repsonse data.
+function populateSearchResults() {
+    // console.log(nationalParks);
+    nationalParks.forEach(function (nationalPark) {
+        // Create rows.
+        var newRow = $("<tr>").append(
+            $("<td>").text(nationalPark.name),
+            $("<td>").text(nationalPark.designation),
+            $("<td>").text(nationalPark.url)
+        );
+        // Append new rows to the table.
+        $("#searchResults > tbody").append(newRow)
+    })
+};
